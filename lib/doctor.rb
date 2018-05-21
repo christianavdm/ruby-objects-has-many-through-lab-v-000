56fdb701b978f8a79d667425a811ee1fde2b69a1
Christiana Vandermale <christianavdm@gmail.com>
2018-05-21 00:42:33 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -10,7 +10,7 @@ class Doctor
   end
   
   def new_appointment(patient, date)
-    new_appointment = Appointment.new(date, patient, self)
+    Appointment.new(date, patient, self)
   end
   
   def self.all
