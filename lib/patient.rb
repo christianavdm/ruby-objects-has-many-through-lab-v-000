9480ff76b6a828df4a8893581373557b7ec0eab5
Christiana Vandermale <christianavdm@gmail.com>
2018-05-21 00:43:44 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -12,4 +12,12 @@ class Patient
   def new_appointment(doctor, date) 
     Appointment.new(date, self, doctor)
   end
+  
+  def appointments
+    Appointment.all.select{|appointment| appointment.patient == self}
+  end
+  
+  def doctors
+    self.appointments.collect{|appointment| appointment.doctor}
+  end
 end
