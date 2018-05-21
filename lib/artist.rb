3991317a4a5e6561782404cdce880bdf252641c7
Christiana Vandermale <christianavdm@gmail.com>
2018-05-21 00:07:35 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,9 +17,8 @@ class Artist
   end
   
   def new_song(name, genre)
-    song = Song.new(name, genre)
+    song = Song.new(name, self, genre)
     @songs << song
-    song.artist = self
     song
   end
   
